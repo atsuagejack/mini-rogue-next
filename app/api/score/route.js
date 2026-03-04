f8b726e3afa51671cbf611b7b18ddb65210e5263
atsuagejack <65150335+atsuagejack@users.noreply.github.com>
2026-03-04 17:49:18 +0900

    Update route.js

--- app/api/score/route.js
+++ app/api/score/route.js
@@ -1,3 +1,10 @@
+let score = 0;
+
 export async function GET() {
-  return Response.json({ score: 100 });
+  return Response.json({ score });
+}
+
+export async function POST() {
+  score += 1;
+  return Response.json({ score });
 }
